67a05a147de6cde06e2796c49ed28568883029e5
Mahad Waseem <mahadwaseem@Mahads-MacBook-Air.local>
2023-06-15 02:26:40 +0500

    adding redirect url

--- document_store/docs_store/urls.py
+++ document_store/docs_store/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.shortcuts import redirect
 from .views import FolderList, FolderDetail, DocumentList, DocumentDetail, TopicList, TopicDetail, FolderDocumentTopicView
 
 
 urlpatterns = [
     path('folders/', FolderList.as_view()),
+    path('', lambda req: redirect('/folders/')),
     path('folders/<int:pk>/', FolderDetail.as_view()),
     path('documents/', DocumentList.as_view()),
     path('documents/<int:pk>/', DocumentDetail.as_view()),
